e68ceaabeba0926547e38d09be9041f4ce937047
Nick Gray <nickjoegray@gmail.com>
2013-04-17 11:59:22 -0400

    another commit

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ MusicRemix::Application.routes.draw do
 
   # devise_for :users, :path => "auth", :path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :unlock => 'unblock', :registration => 'register', :sign_up => 'cmon_let_me_in' }
   # resources :instruments
-  # get "/" => "instruments#redirect"
+  get "/" => "instruments#home"
 
   resources :instrument_category do 
     resources :instruments 
